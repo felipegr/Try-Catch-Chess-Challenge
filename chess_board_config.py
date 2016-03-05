f31aa9a39c692fe0fb18d2efc2bcde72c9eeed4d
felipegr <felipe.rosario@gmail.com>
2016-03-05 12:04:45 -0300

    Pylint adaptations
    Some changes to get a good Pylint score

--- chess_board_config.py
+++ chess_board_config.py
@@ -1,3 +1,22 @@
+# -*- coding: cp1252 -*-
+"""Try Catch's Chess Challenge
+The problem is to find all unique configurations of a set of normal chess
+pieces on a chess board with dimensions M×N where none of the pieces is in
+a position to take any of the others.
+The colour of the piece does not matter, and there are no pawns
+among the pieces.
+
+Input arguments:
+x -- Number of squares of the X dimension of the board
+y -- Number of squares of the Y dimension of the board
+k -- Number of Kings to be placed on the board
+q -- Number of Queens to be placed on the board
+b -- Number of Bishops to be placed on the board
+r -- Number of Rooks to be placed on the board
+n -- Number of Knights to be placed on the board
+verbose -- If present, prints the comfigurations to the screen
+"""
+
 import itertools
 import argparse
 import sys
@@ -18,18 +37,19 @@ def main():
     sys.exit()
 
 
-def find_configurations(x, y, k, q, b, r, n, verbose):
+def find_configurations(x_dim, y_dim, kings, queens, bishops, rooks,
+                        knights, verbose):
     """Find and print all configurations for a chess board where no piece
     threats another.
 
     Keyword arguments:
-    x -- Number of squares of the X dimension of the board
-    y -- Number of squares of the Y dimension of the board
-    k -- Number of Kings to be placed on the board
-    q -- Number of Queens to be placed on the board
-    b -- Number of Bishops to be placed on the board
-    r -- Number of Rooks to be placed on the board
-    n -- Number of Knights to be placed on the board
+    x_dim -- Number of squares of the X dimension of the board
+    y_dim -- Number of squares of the Y dimension of the board
+    kings -- Number of Kings to be placed on the board
+    queens -- Number of Queens to be placed on the board
+    bishops -- Number of Bishops to be placed on the board
+    rooks -- Number of Rooks to be placed on the board
+    knights -- Number of Knights to be placed on the board
 
     Returns the number of configurations found.
     """
@@ -38,16 +58,16 @@ def find_configurations(x, y, k, q, b, r, n, verbose):
 
     # Create possible positions for every piece
     iterators = []
-    for i in range(q):
-        iterators.append(itertools.product('Q', range(x), range(y)))
-    for i in range(b):
-        iterators.append(itertools.product('B', range(x), range(y)))
-    for i in range(r):
-        iterators.append(itertools.product('R', range(x), range(y)))
-    for i in range(k):
-        iterators.append(itertools.product('K', range(x), range(y)))
-    for i in range(n):
-        iterators.append(itertools.product('N', range(x), range(y)))
+    for _ in range(queens):
+        iterators.append(itertools.product('Q', range(x_dim), range(y_dim)))
+    for _ in range(bishops):
+        iterators.append(itertools.product('B', range(x_dim), range(y_dim)))
+    for _ in range(rooks):
+        iterators.append(itertools.product('R', range(x_dim), range(y_dim)))
+    for _ in range(kings):
+        iterators.append(itertools.product('K', range(x_dim), range(y_dim)))
+    for _ in range(knights):
+        iterators.append(itertools.product('N', range(x_dim), range(y_dim)))
 
     # Get combinations of first piece
     piece_iter = iterators.pop(0)
@@ -60,36 +80,37 @@ def find_configurations(x, y, k, q, b, r, n, verbose):
         valid_combs_list = set()
 
         # Loop for these two pieces
-        for c in combs:
-            v, vc = valid_combination(c)
+        for comb in combs:
+            is_valid, valid_comb = valid_combination(comb)
 
             # If it's a valid combination
-            if v:
+            if is_valid:
                 if len(valid_combs_list) == 0:
-                    valid_combs_list.add(vc)
+                    valid_combs_list.add(valid_comb)
                 else:
                     # Don't include duplicates
                     inc = True
-                    check_combs = itertools.permutations(vc, len(vc))
-                    for cc in check_combs:
-                        if cc in valid_combs_list:
+                    check_combs = itertools.permutations(valid_comb,
+                                                         len(valid_comb))
+                    for check_comb in check_combs:
+                        if check_comb in valid_combs_list:
                             inc = False
                             break
 
                     if inc:
-                        valid_combs_list.add(vc)
+                        valid_combs_list.add(valid_comb)
 
         comb_num = len(valid_combs_list)
         piece_iter = itertools.chain(valid_combs_list)
 
     # Gets number of combinations if not set
-    if (comb_num < 0):
+    if comb_num < 0:
         comb_num = sum(1 for x in piece_iter)
 
-    if (verbose):
+    if verbose:
         # Set final combinations to print
         final_combinations = piece_iter
-        
+
         print ''
         print 'Combinations: '
 
@@ -117,11 +138,11 @@ def valid_combination(combination):
     first_piece = combination[0]
 
     # If the rest is just one piece
-    if type(combination[1][0]) != tuple:
+    if not isinstance(combination[1][0], tuple):
         pieces = [combination[1]]
     else:
         pieces = list(combination[1])
-    
+
     return_combination = list(pieces)
     return_combination.append(combination[0])
 
@@ -154,29 +175,29 @@ def threats(first_piece, second_piece):
         return True
 
     # Same column or line: Queens and Rooks
-    if (first_piece[0] in ('Q', 'R')):
+    if first_piece[0] in ('Q', 'R'):
         if (first_piece[1] == second_piece[1] or
                 first_piece[2] == second_piece[2]):
             return True
 
     # Diagonal: Queens and Bishops
-    if (first_piece[0] in ('Q', 'B')):
+    if first_piece[0] in ('Q', 'B'):
         if (abs(second_piece[1] - first_piece[1]) ==
                 abs(second_piece[2] - first_piece[2])):
             return True
 
     # King
-    if (first_piece[0] == 'K'):
+    if first_piece[0] == 'K':
         if (abs(second_piece[1] - first_piece[1]) in (0, 1) and
                 abs(second_piece[2] - first_piece[2]) in (0, 1)):
             return True
 
     # Knight
-    if (first_piece[0] == 'N'):
+    if first_piece[0] == 'N':
         if ((abs(second_piece[1] - first_piece[1]) == 2 and
-            abs(second_piece[2] - first_piece[2]) == 1) or
-            (abs(second_piece[1] - first_piece[1]) == 1 and
-            abs(second_piece[2] - first_piece[2]) == 2)):
+             abs(second_piece[2] - first_piece[2]) == 1) or
+                (abs(second_piece[1] - first_piece[1]) == 1 and
+                 abs(second_piece[2] - first_piece[2]) == 2)):
             return True
 
 
@@ -212,7 +233,7 @@ if __name__ == "__main__":
     if args.x <= 0 or args.y <= 0:
         print "Dimensions must be both greater than zero."
         sys.exit()
-    
+
     # Check if there are pieces to be placed
     if total_pieces == 0:
         print "There aren't pieces to be placed."
